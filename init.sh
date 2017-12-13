daedf739b9128d6d38c3c965ef8533dddfe0a119
amilaupathissa <amilaupathissa@gmail.com>
2017-12-13 14:29:41 +0530

    add - init to master

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Author : Amila Upathissa
+#Date : 2017-12-13
+#Perpose : Init basic system by installing
+#		1). figlet - this is for create text art
+#
+
+
+#install git
+sudo apt-get install git
+		
+echo "Installing Figlet"
+sudo apt-get install figlet  
+
+#Display Msg "Updating"
+figlet Updating
+sudo apt-get update
+
+#Display Msg "Upgrading"
+figlet Upgrading
+sudo apt-get upgrade -y
+
+#End
